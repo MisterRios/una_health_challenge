7d40fce1e42772d0790d7dd2d23720c15eacad8d
daniel rios <misterrios@gmail.com>
2025-05-07 12:22:36 +0200

    feat: add django extensions

--- settings.py
+++ settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "users",
-    "glucose_levels"
+    "glucose_levels",
+    "django_extensions",
 ]
 
 MIDDLEWARE = [
