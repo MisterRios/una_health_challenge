c30d665d4b1b2713ec8238f24103c384a76fbfe7
daniel rios <misterrios@gmail.com>
2025-05-07 12:43:42 +0200

    feat: add api initial views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
-# Create your views here.
+from glucose_levels.models import GlucoseLevel
+from glucose_levels.serializers import GlucoseLevelSerializer
+
+
+class LevelsListView(ListAPIView):
+    model = GlucoseLevel
+    serializer_class = GlucoseLevel
+    queryset = GlucoseLevel.objects.all()
+
+
+# class LevelsItemView(RetrieveAPIView):
+#     model = GlucoseLevel
+#     serializer_class = GlucoseLevel
+#     queryset = GlucoseLevel.objects.all()
